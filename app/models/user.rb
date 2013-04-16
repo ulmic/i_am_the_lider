4fae82fdf81983b2090fca505b3d93d77aae1e72
kalashnikovisme <kalashnikovisme@gmail.com>
2013-04-16 08:45:49 +0400

    add model and views on haml

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ActiveRecord::Base
+  attr_accessible :birth_date, :district_id, :email, :first_name, :group, :home_phone, :last_name, :login, :middle_name, :mobile_phone, :password, :school, :twitter, :vkontakte
+end
