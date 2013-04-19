775217189b253d98b25be2f9a49bd3d1ffb87ee4
kalashnikovisme <kalashnikovisme@gmail.com>
2013-04-20 02:17:03 +0400

    add validation to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ActiveRecord::Base
   attr_accessible :date, :description, :goal, :place, :results, :target_audience, :tasks, :title
 
+  validates_presence_of :date, :description, :goal, :place, :results, :target_audience, :tasks, :title
+  validates_length_of :goal, :is => 140
   belongs_to :user
 end
