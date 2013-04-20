5cca4ce1b916182a2c03b15a897a0a1580db4d43
kalashnikovisme <kalashnikovisme@gmail.com>
2013-04-20 05:03:58 +0400

    add radius divs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @users = User.all
-
+    @projects = Project.last(4)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @page }
