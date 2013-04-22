991fcc9adb2f1bfa3710e544ccab7373ee1ac50a
kalashnikovisme <kalashnikovisme@gmail.com>
2013-04-22 09:04:29 +0400

    remove conflict

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,19 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-<<<<<<< HEAD
-<<<<<<< HEAD
-    @users = User.all
-
-=======
     @users_first = User.first(3)
     @users_last = User.last(3)
     @projects = Project.first(4)
->>>>>>> make_up
-=======
-    @users_first = User.first(3)
-    @users_last = User.last(3)
-    @projects = Project.first(4)
->>>>>>> make_up
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @page }
