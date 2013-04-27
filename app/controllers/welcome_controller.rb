7e7c4798c8dd0d3696200b3f80f2198f31af1230
aelaa <aelaa@aelaa-N100.(none)>
2013-04-27 14:14:21 +0400

    blog on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,6 @@ class WelcomeController < ApplicationController
     @users_first = User.first(3)
     @users_last = User.last(3)
     @projects = Project.first(4)
-		@last_posts = BlogPost.find(:all, :order => "id desc", :limit => 5).reverse
     respond_to do |format|
       format.html # index.html.erb
     end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,6 +3,14 @@ class BlogPost < ActiveRecord::Base
 
   belongs_to :user
 
+  def preview
+    if self.text.length>140
+      self.text[0..137]+"..." 
+    else
+      self.text
+    end
+  end
+
   def get_html_text
     self.text.bbcode_to_html
   end
