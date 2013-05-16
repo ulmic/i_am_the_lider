da60cb6ff1b2cdbb09008f4601f906281f67e39e
kalashnikovisme <kalashnikovisme@gmail.com>
2013-05-17 03:05:50 +0400

    add unless

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   include AuthHelper
   def check_if_admin
-    if !admin_signed_in?
+    unless admin_signed_in?
       render_404
     end
   end
 
   def check_current_user
-    if !user_signed_in?
+    unless user_signed_in?
       render_404
     end
   end
