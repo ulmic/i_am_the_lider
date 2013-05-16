27ff68abe544b06dbe3fdd6eb7f5b8111afda004
kalashnikovisme <kalashnikovisme@gmail.com>
2013-05-17 02:40:36 +0400

    remove captains comments and trash from news controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,84 +1,45 @@
 class NewsController < ApplicationController
   before_filter :check_if_admin, only: [:edit, :update, :destroy, :create, :new]
 
-  # GET /news
-  # GET /news.json
   def index
     @news = News.all.reverse
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @news }
-    end
   end
 
-  # GET /news/1
-  # GET /news/1.json
   def show
     @news = News.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @news }
-    end
   end
 
-  # GET /news/new
-  # GET /news/new.json
   def new
     @news = News.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @news }
-    end
   end
 
-  # GET /news/1/edit
   def edit
     @news = News.find(params[:id])
   end
 
-  # POST /news
-  # POST /news.json
   def create
     @news = News.new(params[:news])
 
-    respond_to do |format|
-      if @news.save
-        format.html { redirect_to @news, notice: 'News was successfully created.' }
-        format.json { render json: @news, status: :created, location: @news }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @news.errors, status: :unprocessable_entity }
-      end
+    if @news.save
+      redirect_to @news
+    else
+      render action: "new"
     end
   end
 
-  # PUT /news/1
-  # PUT /news/1.json
   def update
     @news = News.find(params[:id])
 
-    respond_to do |format|
-      if @news.update_attributes(params[:news])
-        format.html { redirect_to @news, notice: 'News was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @news.errors, status: :unprocessable_entity }
-      end
+    if @news.update_attributes(params[:news])
+      redirect_to @news
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /news/1
-  # DELETE /news/1.json
   def destroy
     @news = News.find(params[:id])
     @news.destroy
-
-    respond_to do |format|
-      format.html { redirect_to news_index_url }
-      format.json { head :no_content }
-    end
+    redirect_to news_index_url
   end
 end
