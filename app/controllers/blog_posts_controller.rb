a07b8104fecb59ca7b61733c3a04b247f072caa2
kalashnikovisme <kalashnikovisme@gmail.com>
2013-05-17 02:53:52 +0400

    remove trash from blog_posts controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -27,38 +27,25 @@ class BlogPostsController < ApplicationController
   def create
     @blog_post = BlogPost.new(params[:blog_post])
     @blog_post.user_id = session[:user_id]
-    respond_to do |format|
-      if @blog_post.save
-        format.html { redirect_to :office, notice: 'Запись в блог добавлена.' }
-        format.json { render json: @blog_post, status: :created, location: @blog_post }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @blog_post.errors, status: :unprocessable_entity }
-      end
+    if @blog_post.save
+      redirect_to :office, notice: 'Запись в блог добавлена.'
+    else
+      render action: "new"
     end
   end
 
   def update
     @blog_post = BlogPost.find(params[:id])
-
-    respond_to do |format|
-      if @blog_post.update_attributes(params[:blog_post])
-        format.html { redirect_to @blog_post, notice: 'Запись в блоге обновлена.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @blog_post.errors, status: :unprocessable_entity }
-      end
+    if @blog_post.update_attributes(params[:blog_post])
+      redirect_to @blog_post, notice: 'Запись в блоге обновлена.'
+    else
+      render action: "edit"
     end
   end
 
   def destroy
     @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to blog_posts_url }
-      format.json { head :no_content }
-    end
+    redirect_to blog_posts_url
   end
 end
