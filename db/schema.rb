2b9d4b24f7e3c560c9d152a63415996aa4e39150
kalashnikovisme <kalashnikovisme@gmail.com>
2013-05-25 00:38:58 +0400

    removed confirm from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521234916) do
+ActiveRecord::Schema.define(:version => 20130524203658) do
 
   create_table "admins", :force => true do |t|
     t.string   "login"
@@ -116,7 +116,6 @@ ActiveRecord::Schema.define(:version => 20130521234916) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.boolean  "confirm"
   end
 
 end
