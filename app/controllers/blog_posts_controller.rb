f020d58fb3d7dc0063d3da6a1fe01feb96529390
aelaa <aelaau@gmail.com>
2013-05-25 14:31:15 +0400

    blog posts test fixed

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 class BlogPostsController < ApplicationController
-  before_filter :check_user_signed, only: [:edit, :update, :destroy, :create, :new]
+  before_filter :check_user_signed, only: [:new, :create]
 
   def index
     @blog_posts = BlogPost.all
