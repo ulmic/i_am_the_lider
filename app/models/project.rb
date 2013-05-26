87f35c00aa62fd66a217bd1d15fea716cd03b836
kalashnikovisme <kalashnikovisme@gmail.com>
2013-05-26 22:14:29 +0400

    remove projects files

--- app/models/project.rb
+++
@@ -1,7 +0,0 @@
-class Project < ActiveRecord::Base
-  attr_accessible :date, :description, :goal, :place, :results, :target_audience, :tasks, :title, :user_id
-
-  validates_presence_of :date, :description, :goal, :place, :results, :target_audience, :tasks, :title
-  validates_length_of :goal, :maximum => 140
-  belongs_to :user
-end
