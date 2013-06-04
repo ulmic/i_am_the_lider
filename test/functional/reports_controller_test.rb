dc2f2ae83e1cb7d1672629f3dc89f3d1e3372936
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-06-05 00:29:23 +0400

    fix bug in reports_controller_test

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -16,7 +16,7 @@ class ReportsControllerTest < ActionController::TestCase
 
   test "should get edit report by admin" do
     admin_sign_in @admin
-    
+
     get :edit, id: @report
     assert_response :success
   end
@@ -24,14 +24,14 @@ class ReportsControllerTest < ActionController::TestCase
   test "should get edit report by user" do
     @user.id = @report.user_id
     user_sign_in @user
-    
+
     get :edit, id: @report
     assert_response :success
   end
 
   test "should create report" do
     user_sign_in @user
-    
+
     attributes = attributes_for :report
     post :create, report: attributes
     assert_response :redirect
@@ -54,23 +54,23 @@ class ReportsControllerTest < ActionController::TestCase
   test "should update report" do
     @user.id = @report.user_id
     user_sign_in @user
-    
+
     attributes = attributes_for :report
     put :update, id: @report, report: attributes
     assert_response :redirect
-  
+
     @report.reload
     assert_equal attributes[:description], @report.description
   end
-  
+
   test "should destroy report" do
     @user.id = @report.user_id
     user_sign_in @user
-  
+
     assert_difference('Report.count', -1) do
       delete :destroy, id: @report
     end
-  
+
     assert_redirected_to reports_path
   end
 
@@ -91,13 +91,13 @@ class ReportsControllerTest < ActionController::TestCase
   test "should not update report with no access" do
     attributes = attributes_for :report
     put :update, id: @report, report: attributes
-    
+
     assert_redirected_to @report.user
   end
 
   test "should get_viewed_user_id" do
     admin_sign_in @admin
-    set_viewed_user_id @user
+    set_viewed_user @user
 
     attributes = attributes_for :report
     post :create, report: attributes
