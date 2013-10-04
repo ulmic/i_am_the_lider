ae8b59a05223322e96e16ec037592145a6b4742d
pavel <kalashnikovisme@gmail.com>
2013-10-04 11:18:27 +0400

    fix photo uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -9,12 +9,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  version :thumb do
-    process resize_to_fill: [100, 100]
-  end
-  version :big_thumb do
-    process resize_to_fill: [300, 300]
-  end
   def extension_white_list
     %w(jpg jpeg gif png)
   end
