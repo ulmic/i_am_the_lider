7daefedb5c3a93586ebf21916adb7db6394e2eb7
pavel <kalashnikovisme@gmail.com>
2013-12-24 00:40:39 +0400

    fix admin_welcome functional

--- test/functional/admin/welcome_controller_test.rb
+++ test/functional/admin/welcome_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class Admin::WelcomeControllerTest < ActionController::TestCase
+  setup do
+    @admin = create :admin
+    admin_sign_in @admin
+  end
+
   test "should get index" do
     get :index
     assert_response :success
