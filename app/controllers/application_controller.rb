006069cd46e0b014afed99456a81e64ac3584631
pavel <kalashnikovisme@gmail.com>
2013-12-24 00:59:50 +0400

    remove admin functional from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
   include AuthHelper
 
-  def check_if_admin
-    unless admin_signed_in?
-      redirect_to not_found_errors_path
-    end
-  end
-
   def check_user_signed
     unless user_signed_in?
       redirect_to not_found_errors_path
