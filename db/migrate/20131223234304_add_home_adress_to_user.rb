35d51275f02bcd044b25e52373bd9d2edc1980c6
pavel <kalashnikovisme@gmail.com>
2013-12-24 06:04:54 +0400

    fix migration

--- db/migrate/20131223234304_add_home_adress_to_user.rb
+++ db/migrate/20131223234304_add_home_adress_to_user.rb
@@ -1,6 +1,6 @@
 class AddHomeAdressToUser < ActiveRecord::Migration
   def change
     add_column :users, :adress_index, :integer
-    add_column :users, :locality, :string
+    add_column :users, :locality, :text
   end
 end
