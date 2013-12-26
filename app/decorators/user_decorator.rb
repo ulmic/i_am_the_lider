ee241937c8b15e38b9e4a0b5c07d8809b2c0291a
pavel <kalashnikovisme@gmail.com>
2013-12-26 23:20:43 +0400

    remove function from decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -9,15 +9,4 @@ class UserDecorator < Draper::Decorator
     "#{model.first_name} #{model.middle_name}"
   end
 
-  def confirm_state_locales
-    if model.confirm_state == "new"
-      I18n.t('new_user')
-    elsif model.confirm_state == "accepted"
-      I18n.t('accepted_user')
-    elsif model.confirm_state == "reserved"
-      I18n.t('reserver_user')
-    elsif model.confirm_state == "busted"
-      I18n.t('busted_user')
-    end
-  end
 end
