91a80e072b88bec646a349ab2a4e40e24d8d3ea0
pavel <kalashnikovisme@gmail.com>
2013-12-27 05:51:47 +0400

    add ui to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new params[:user]
     if @user.save
+      user_sign_in @user
       redirect_to @user, flash: :success
     else
       render action: :new, flash: :error
