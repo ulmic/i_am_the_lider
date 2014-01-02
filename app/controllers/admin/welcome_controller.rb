aea72b47d88007be13901b0f35c224525808b121
pavel <kalashnikovisme@gmail.com>
2014-01-03 01:53:22 +0400

    add access to not_accepted users

--- app/controllers/admin/welcome_controller.rb
+++ app/controllers/admin/welcome_controller.rb
@@ -1,8 +1,5 @@
 class Admin::WelcomeController < Admin::ApplicationController
   def index
-    unless admin_signed_in?
-      redirect_to new_admin_session_path
-    end
     @new_users = UserDecorator.decorate_collection User.with_confirm_state :new
   end
 end
