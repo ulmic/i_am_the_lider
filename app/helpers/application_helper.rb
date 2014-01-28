bc8480d80ca146ef413ca67c3c92849364d1c6b2
pavel <kalashnikovisme@gmail.com>
2014-01-28 22:42:39 +0400

    add video to main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   include AuthHelper
+  include AutoHtml
 
   def copyright_notice_year_range(start_year)
     start_year = start_year.to_i
@@ -16,6 +17,14 @@ module ApplicationHelper
   def current_user_on_page?(user)
     if current_user
       current_user.id === user.id
+    end
+  end
+
+  def youtube_video(link)
+    if link
+      auto_html link do
+        youtube width: 384, height: 240
+      end
     end
   end
 end
