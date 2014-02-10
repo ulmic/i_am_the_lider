2415e18d20343ef87d97f4b9a673a60be9566860
pavel <kalashnikovisme@gmail.com>
2014-02-10 19:53:45 +0400

    edit config

--- lib/configus.rb
+++ lib/configus.rb
@@ -1,6 +1,6 @@
 Configus.build :development do
   env :production do
-    end_reg_time '2014-02-10 00:00:00'
+    end_reg_time '2014-02-11 00:00:00'
   end
   env :development, parent: :production
   env :test, parent: :development
