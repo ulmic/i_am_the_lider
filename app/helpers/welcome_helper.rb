6d970fac042658de1a6878a7cd2fc24e6a6eddc9
pavel <kalashnikovisme@gmail.com>
2014-02-12 02:14:00 +0400

    fix admin/stages form

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,5 +1,10 @@
 module WelcomeHelper
-  def days_to_market_dream
+  def market_dreams_coming_soon?
+    raise Stage.find_by_title("Ярмарка идей").begin_date.inspect
+    Date.today < Stage.find_by_title("Ярмарка идей").begin_date
+  end
+
+  def days_to_market_dreams
     Stage.find_by_title("Ярмарка идей").begin_date - Date.today
   end
 end
