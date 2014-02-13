8e54252f6b68ce3d313eb3f792507335a8eef615
pavel <kalashnikovisme@gmail.com>
2014-02-13 15:03:32 +0400

    fix tests

--- test/functional/admin/ratings_controller_test.rb
+++ test/functional/admin/ratings_controller_test.rb
@@ -4,10 +4,11 @@ class Admin::RatingsControllerTest < ActionController::TestCase
   setup do
     @admin = create :admin
     admin_sign_in @admin
+    @stage = create :stage
   end
 
   test "should get index" do
-    get :index
+    get :index, id: @stage
     assert_response :success
   end
 end
