052127fbed896eb59a24a7eb8be7a52306eeb939
pavel <kalashnikovisme@gmail.com>
2014-02-17 00:03:20 +0400

    add available in the chrome web store

--- lib/configus.rb
+++ lib/configus.rb
@@ -1,9 +1,14 @@
 Configus.build Rails.env do
   env :production do
     end_reg_time '2014-02-13 00:00:00'
+
     market_dreams do
       apply_part "http://www.ulmic.ru/programms/lider/3040--2014"
       end_reg_time '2014-02-16 00:00:00'
+    end
+
+    apps do
+      chrome_web_store_url "https://chrome.google.com/webstore/detail/%D1%8F-%D0%BB%D0%B8%D0%B4%D0%B5%D1%80/lehbmnengpcoaledhaodhjcmibleffmd/"
     end
   end
   env :development, parent: :production
