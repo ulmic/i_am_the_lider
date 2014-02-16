e719650b5081308a516b1b2cff35a602f73c4cf4
pavel <kalashnikovisme@gmail.com>
2014-02-16 21:46:23 +0400

    add end_reg_to_market_dreams

--- lib/configus.rb
+++ lib/configus.rb
@@ -1,7 +1,10 @@
 Configus.build Rails.env do
   env :production do
     end_reg_time '2014-02-13 00:00:00'
-    apply_part_in_market_dream_url "http://www.ulmic.ru/programms/lider/3040--2014"
+    market_dreams do
+      apply_part "http://www.ulmic.ru/programms/lider/3040--2014"
+      end_reg_time '2014-02-16 00:00:00'
+    end
   end
   env :development, parent: :production
   env :test, parent: :development
