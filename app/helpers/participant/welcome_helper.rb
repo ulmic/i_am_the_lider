be4b456a8f4ac0d8f82513d2e03f5d68b2726ced
pavel <kalashnikovisme@gmail.com>
2014-03-13 04:03:31 +0400

    add functional for losers

--- app/helpers/participant/welcome_helper.rb
+++ app/helpers/participant/welcome_helper.rb
@@ -1,2 +1,6 @@
 module Participant::WelcomeHelper
+  def participant_is_semifinalist?(user)
+    stage = Stage.find_by_title(I18n.t('stages.time_of_real_deals'))
+    user.last_stage_id >= stage.id
+  end
 end
