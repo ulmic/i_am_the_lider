494dff0de3ed6e7a1e19d6bfb47d794099dba1b6
pavel <kalashnikovisme@gmail.com>
2014-03-17 03:04:47 +0400

    fix map

--- app/assets/javascripts/participant/geo_marker.js
+++ app/assets/javascripts/participant/geo_marker.js
@@ -1,22 +1,41 @@
 var map;
 var marker;
-var clickFlag = 0;
-var myCenter = new google.maps.LatLng(54.2863343, 48.3477731);
+var click_flag = 0;
+var my_center = new google.maps.LatLng(54.2863343, 48.3477731);
 
 function init_my_center(latitude, longitude) {
-  myCenter = new google.maps.LatLng(latitude, longitude);
+  my_center = new google.maps.LatLng(latitude, longitude);
+  var mapProp = {
+    center: my_center,
+    zoom: 10,
+    mapTypeControl: true,
+    sensor: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  google.maps.event.addListener(map, 'click', function(event) {
+    place_marker(event.latLng);
+  });
+  marker = new google.maps.Marker({
+    position: "(" + latitude + ", " + longitude + ")",
+    animation: google.maps.Animation.BOUNCE,
+    map: map,
+  });
 }
 
 function initialize() {
   var mapProp = {
-    center:myCenter,
-    zoom:10,
-    mapTypeControl:true,
-    sensor:true,
+    center: my_center,
+    zoom: 10,
+    mapTypeControl: true,
+    sensor: true,
     mapTypeControlOptions: {
-      style:google.maps.MapTypeControlStyle.DROPDOWN_MENU
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
     },
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
   google.maps.event.addListener(map, 'click', function(event) {
@@ -26,13 +45,13 @@ function initialize() {
 }
 
 function place_marker(location) {
-  if (clickFlag === 0) {
+  if (click_flag === 0) {
     marker = new google.maps.Marker({
       position: location,
-      animation:google.maps.Animation.BOUNCE,
+      animation: google.maps.Animation.BOUNCE,
       map: map,
     });
-    clickFlag = 1;
+    click_flag = 1;
   } else {
     marker.setPosition(location);
     map.panTo(location);
