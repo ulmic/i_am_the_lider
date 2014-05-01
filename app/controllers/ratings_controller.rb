6dba47c1b35dfd180d816d8c62ed955c8d21f4ea
pavel <kalashnikovisme@gmail.com>
2014-05-01 09:05:49 +0400

    fixed

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,10 @@
 class RatingsController < ApplicationController
   def index
-    @users = UserDecorator.decorate_collection User.where("last_stage_id >= ?", params[:id])
     @stage = Stage.find params[:id]
+    if @stage.published?
+      @users = UserDecorator.decorate_collection User.where("last_stage_id >= ?", params[:id])
+    else
+      redirect_to not_found_errors_path
+    end
   end
 end
